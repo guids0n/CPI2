0925f2a2d9ea8b944b61be08984c59190ceaf3fc
guids0n <guidsonbarreto@gmail.com>
2025-12-19 20:40:41 +0000

    Adiciona diretórios src e public do projeto Vue

---
+++ projeto_guidson/src/App.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import CalculadoraSoma from './components/CalculadoraSoma.vue'
+</script>
+
+<template>
+  <div class="tela">
+    <h1>Projeto por Guidson Barreto de Jesus Silva</h1>
+    <CalculadoraSoma />
+  </div>
+</template>
+
+<style>
+body {
+  margin: 0;
+  background-color: #8be0b6;
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+}
+
+.tela {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  min-height: 100vh;
+}
+
+h1 {
+  color: #444;
+  margin-bottom: 20px;
+}
+</style>
---
+++ projeto_guidson/src/components/CalculadoraSoma.vue
@@ -0,0 +1,76 @@
+<script setup lang="ts">
+import { ref, computed } from 'vue'
+
+const numero1 = ref(0)
+const numero2 = ref(0)
+
+const resultado = computed(() => {
+  return numero1.value + numero2.value
+})
+</script>
+
+<template>
+  <div class="calculadora">
+    <h2>➕ Somadora Vue</h2>
+
+    <div class="inputs">
+      <input type="number" v-model.number="numero1" placeholder="0" />
+      <span class="sinal">+</span>
+      <input type="number" v-model.number="numero2" placeholder="0" />
+    </div>
+
+    <div class="resultado-box">
+      <p>Resultado:</p>
+      <strong class="valor">{{ resultado }}</strong>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.calculadora {
+  background-color: #f8f9fa;
+  border: 2px solid #074d38;
+  border-radius: 12px;
+  padding: 30px;
+  max-width: 350px;
+  margin: 0 auto;
+  text-align: center;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  color: #333;
+}
+
+.inputs {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 10px;
+  margin-bottom: 20px;
+}
+
+input {
+  width: 80px;
+  padding: 10px;
+  font-size: 18px;
+  text-align: center;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+}
+
+.sinal {
+  font-weight: bold;
+  font-size: 24px;
+  color: #2c8f39;
+}
+
+.resultado-box {
+  background-color: #16d39a;
+  color: white;
+  padding: 15px;
+  border-radius: 8px;
+}
+
+.valor {
+  font-size: 32px;
+  display: block;
+}
+</style>
